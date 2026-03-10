b838ad0ee71c31e3d176f4862555a0dbbc395a1f
Razqa <razqaazaki55@gmail.com>
2026-03-10 14:31:45 +0700

    Modul 3

--- PreTest/Donor.java
+++ PreTest/Donor.java
@@ -1,10 +1,38 @@
 import java.util.Scanner;
 
-public class Donor {
+public class Donor {   
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Masukkan usia Anda: ");
+        int usia = input.nextInt();
+
+        System.out.print("Masukkan berat badan Anda (kg): ");
+        float beratBadan = input.nextFloat();
+
+        System.out.print("Masukkan kadar hemoglobin Anda (angka desimal misal 10,5 g/dl): ");
+        float kadarHemoglobin = input.nextFloat();
+
+        if (usia >= 17) {
+            System.out.println("Usia memenuhi syarat.");
+
+            if (beratBadan >= 50) {
+                System.out.println("Berat badan memenuhi syarat.");
+
+                if (kadarHemoglobin >= 12.5) {
+                    System.out.println("Kadar hemoglobin memenuhi syarat dan Anda diperbolehkan donor darah.");
+                } else {
+                    System.out.println("Kadar hemoglobin tidak memenuhi syarat. Anda tidak diperbolehkan donor darah.");
+                }
+
+            } else {
+                System.out.println("Berat badan tidak memenuhi syarat. Berat badan minimal 50 kg.");
+            }
+
+        } else {
+            System.out.println("Usia kurang dari 17 tahun, tidak diperbolehkan donor darah.");
+        }
+
+        input.close();
     }
-    
 }
